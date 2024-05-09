db722a75322d8f8c94dc305119a9054718801079
Henry Beveridge <70719277+Illuminate-dev@users.noreply.github.com>
2024-05-09 05:27:40 +0000

    poc extracting data

--- main.lua
+++ main.lua
@@ -1,10 +1,21 @@
 local http = require('socket.http')
 local parser = require('htmlparser')
+local headers = {"id", "author", "title", "publisher", "year", "pages", "language", "size", "filetype", "link 1"}
+
+
 res = http.request("https://libgen.is/search.php?req=ap+world+history&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def")
 local root = parser.parse(res, 5000)
-local elements = root:select("tr td a")
+local elements = root:select("table")
 print(#elements)
 
-for _,e in ipairs(elements) do
-	print(e:getcontent())
+local subs = elements[2]("tr")
+
+for i, sub in ipairs(subs) do
+	if i == 1 then goto continue end
+	for j, entry in ipairs(sub("td")) do
+		if j > #headers then goto continue end
+		print(headers[j] .. " " .. entry:getcontent())
+	end
+	
+	::continue::
 end
