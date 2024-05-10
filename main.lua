cac3b93a4430a5f461761a1a2867e21a6b430f00
Henry Beveridge <70719277+Illuminate-dev@users.noreply.github.com>
2024-05-10 05:04:02 +0000

    finish basic scraper

--- main.lua
+++ main.lua
@@ -2,20 +2,45 @@ local http = require('socket.http')
 local parser = require('htmlparser')
 local headers = {"id", "author", "title", "publisher", "year", "pages", "language", "size", "filetype", "link 1"}
 
+function Search (term)
+	local link = "https://libgen.is/search.php?req=".. string.gsub(term, " ", "+")
+	print(link)
+	local res = http.request(link)
+	local root = parser.parse(res, 5000)
+	local elements = root:select("table")
 
-res = http.request("https://libgen.is/search.php?req=ap+world+history&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def")
-local root = parser.parse(res, 5000)
-local elements = root:select("table")
-print(#elements)
+	local subs = elements[2]("tr")
 
-local subs = elements[2]("tr")
-
-for i, sub in ipairs(subs) do
-	if i == 1 then goto continue end
-	for j, entry in ipairs(sub("td")) do
-		if j > #headers then goto continue end
-		print(headers[j] .. " " .. entry:getcontent())
+	local entries = {}
+	for i, sub in ipairs(subs) do
+		if i == 1 then goto continue end
+		local entry = {}
+		for j, row in ipairs(sub("td")) do
+			if j > #headers then break end
+			if headers[j] == "author" then
+				entry[headers[j]] = row("a")[1]:getcontent()
+			elseif headers[j] == "title" then
+				local titleElement = row('a[title]')[1]
+				entry[headers[j]] = titleElement:getcontent()
+				for _, child in ipairs(titleElement.nodes) do
+					entry[headers[j]] = string.gsub(entry[headers[j]], "<.*/%a+>", "")
+				end
+			elseif headers[j] == "link 1" then
+				entry[headers[j]] = row("a")[1].attributes["href"]
+			else
+				entry[headers[j]] = row:getcontent()
+			end
+		end
+		table.insert(entries, entry)
+		::continue::
 	end
-	
-	::continue::
+	return entries
+end
+
+local results = Search("test")
+
+for i, entry in ipairs(results) do
+	for _, key in ipairs(headers) do
+		print(key..": "..entry[key])
+	end
 end
